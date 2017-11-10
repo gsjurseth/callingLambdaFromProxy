aa8e4083e5a94c9cdfe383a674fb6d3335ec2572
gsjurseth <geir.sjurseth@gmail.com>
2017-11-10 10:10:04 +0100

    serverless lambda piece

---
+++ myLambda/handler.js
@@ -0,0 +1,14 @@
+const Promise = require('bluebird'),
+				fetch = require('node-fetch');
+
+module.exports.hello = (ev, ctx, cb) => {
+				fetch( 'http://httpbin.org/ip' )
+				.then( d => {
+								console.log('managed to fetch the ip');
+								cb(null,d);
+				})
+				.catch( e => {
+								console.error('failed with error: %s', e.stack);
+								cb( null, {statusCode: 500, msg: e.msg});
+				});
+};
